d4b9db839f6cef4a547927abe92960f2e1b42ada
Julian Lannigan <julian.lannigan@gmail.com>
2012-07-23 03:44:27 -0400

    added winston, added auto maintenance mode for db connection failure

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , user = require('./utilities/user')
-  , mongoose = require('mongoose');
+  , mongoose = require('mongoose')
+  , winston = require('winston');
 
 var app = express();
 
@@ -31,15 +32,37 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', user.checkLogin, routes.index);
-app.get('/login', routes.login);
-app.post('/login', routes.doLogin);
-app.get('/logout', routes.logout);
+app.logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({timestamp: true})
+  ]
+});
 
-mongoose.connect(app.get('mongouri'), function() {
-  console.log('Connected to mongoDB.');
+var checkMaintenanceMode = function(req, res, next) {
+  if (app.get('databaseConnected')) {
+    return next();
+  } else {
+    res.render('maintenance', {reason: 'DB is not connected', title: 'Mini Log Title - Maintenance'});
+  }
+}
 
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-  });
-})
+app.get('/', checkMaintenanceMode, user.checkLogin, routes.index);
+app.get('/login', checkMaintenanceMode, routes.login);
+app.post('/login', checkMaintenanceMode, routes.doLogin);
+app.get('/logout', checkMaintenanceMode, routes.logout);
+
+mongoose.connect(app.get('mongouri'));
+
+http.createServer(app).listen(app.get('port'), function(){
+  app.logger.info("Server listening on port " + app.get('port'));
+});
+
+mongoose.connection.on('open', function(e) {
+  app.set('databaseConnected', true);
+  app.logger.info('Database connected.');
+});
+
+mongoose.connection.on('error', function(e) {
+  app.set('databaseConnected', false);
+  app.logger.error('DB:', e.toString());
+});
